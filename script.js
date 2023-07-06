7296928e2d6ad154ed935bd0af231ca4c7bcdfec
Dave Lilleystone <davelilleystone@gmail.com>
2023-07-06 12:47:22 +0100

    add checkInputIsValid function

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// TODO - check if value has been pasted into input
+
+const binaryInput = document.querySelector('#binary-input');
+const decimalOutput = document.querySelector('#decimal-output');
+
 const binaryToDecimal = (value) => parseInt(value, 2);
 
-console.log(binaryToDecimal('10000000'));
+const checkInputIsValid = (evt) => {
+  if (evt.inputType === 'insertText') {
+    if (evt.data === '0' || evt.data === '1') {
+      console.log('valid input');
+      decimalOutput.value = binaryToDecimal(binaryInput.value);
+    } else {
+      binaryInput.value = binaryInput.value.slice(
+        0,
+        binaryInput.value.length - 1
+      );
+      console.log('invalid input');
+    }
+    return;
+  }
+
+  if (binaryInput.value.length) {
+    decimalOutput.value = binaryToDecimal(binaryInput.value);
+    return;
+  }
+
+  decimalOutput.value = '';
+};
+
+binaryInput.addEventListener('input', checkInputIsValid);
