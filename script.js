a222271625e92e5e4576835ac15baff66b92cd27
Dave Lilleystone <davelilleystone@gmail.com>
2023-07-06 13:10:07 +0100

    add input error handling

--- script.js
+++ script.js
@@ -2,22 +2,27 @@
 
 const binaryInput = document.querySelector('#binary-input');
 const decimalOutput = document.querySelector('#decimal-output');
+const errorDiv = document.querySelector('.error-message');
 
 const binaryToDecimal = (value) => parseInt(value, 2);
 
 const checkInputIsValid = (evt) => {
   if (evt.inputType === 'insertText') {
     if (evt.data === '0' || evt.data === '1') {
-      console.log('valid input');
       decimalOutput.value = binaryToDecimal(binaryInput.value);
+      if (!errorDiv.classList.contains('hidden')) {
+        errorDiv.classList.add('hidden');
+      }
     } else {
       binaryInput.value = binaryInput.value.slice(
         0,
         binaryInput.value.length - 1
       );
-      console.log('invalid input');
+      if (errorDiv.classList.contains('hidden')) {
+        errorDiv.classList.remove('hidden');
+      }
+      return;
     }
-    return;
   }
 
   if (binaryInput.value.length) {
